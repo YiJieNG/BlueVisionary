c88fbef9d1a124a41d2bd55072dbabec67c4d4ad
YiJieNG <ngyijie721@gmail.com>
2024-09-01 23:27:17 +1000

    minigame skeleton implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import MarineLife from "./pages/marinelife/MarineLife";
 import Quiz from "./pages/education/Quiz";
+import MiniGame from "./pages/minigame/MiniGame";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/marinelife" element={<MarineLife />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/minigame" element={<MiniGame />} />
       </Routes>
     </Router>
   );
