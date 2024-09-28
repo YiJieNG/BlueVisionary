e7f4af464d34273725fb0e0d56d578d23c2251f7
YiJieNG <ngyijie721@gmail.com>
2024-09-29 03:51:46 +1000

    UI changed for dashboard

--- frontend/src/pages/tracker/PlasticStackBarChart.js
+++ frontend/src/pages/tracker/PlasticStackBarChart.js
@@ -1,77 +1,85 @@
 // import { useState, useEffect } from "react";
-import { BarChart } from '@mui/x-charts/BarChart';
+import { BarChart } from "@mui/x-charts/BarChart";
 import { Row } from "reactstrap";
 // import trackData from "./plastic.json"
-import { Bar } from "react-chartjs-2";
+// import { Bar } from "react-chartjs-2";
 
-function PlasticStackBarChart({dataset, xLabels}) {
-    const options = {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: "top",
-          },
-          title: {
-            display: false,
-            text: "Stacked Bar Chart Example",
-          },
-        },
-        scales: {
-          x: {
-            stacked: true,
-            title: {
-                display: true,
-                text: "Time"
-            },
-            grid: {
-                display: false
-            }
-          },
-          y: {
-            stacked: true,
-            title: {
-                display: true,
-                text: "Plastic recycle"
-            },
-            // grid: {
-            //     display: false
-            // }
-          },
-        },
-      };
-    return (
-        <>
-            <div style={{ padding: "13px" }}>
-                <Row>
-                    {dataset &&
-                        <BarChart
-                            width={700}
-                            height={400}
-                            series={dataset}
-                            xAxis={[{ data: xLabels, scaleType: 'band' }]}
-                            borderRadius={6}
-                            slotProps={{
-                                legend: {
-                                  labelStyle: {
-                                    fontSize: 10,
-                                    // fill: 'blue',
-                                  },
-                                },
-                              }}
-                        />
-                    }
-                    {/* <Bar 
+function PlasticStackBarChart({ dataset, xLabels }) {
+  // const options = {
+  //   responsive: true,
+  //   plugins: {
+  //     legend: {
+  //       position: "top",
+  //     },
+  //     title: {
+  //       display: false,
+  //       text: "Stacked Bar Chart Example",
+  //     },
+  //   },
+  //   scales: {
+  //     x: {
+  //       stacked: true,
+  //       title: {
+  //         display: true,
+  //         text: "Time",
+  //       },
+  //       grid: {
+  //         display: false,
+  //       },
+  //     },
+  //     y: {
+  //       stacked: true,
+  //       title: {
+  //         display: true,
+  //         text: "Plastic recycle",
+  //       },
+  //       // grid: {
+  //       //     display: false
+  //       // }
+  //     },
+  //   },
+  // };
+  return (
+    <>
+      <div style={{ padding: "13px" }}>
+        <Row style={{ height: "400px", width: "100%" }}>
+          {dataset && (
+            <BarChart
+              series={dataset}
+              xAxis={[
+                {
+                  data: xLabels,
+                  scaleType: "band",
+                  // label: "Time", // X-axis label
+                },
+              ]}
+              // yAxis={[
+              //   {
+              //     label: "Plastic recycled", // Y-axis label
+              //   },
+              // ]}
+              borderRadius={6}
+              slotProps={{
+                legend: {
+                  labelStyle: {
+                    fontSize: 10,
+                  },
+                },
+              }}
+            />
+          )}
+        </Row>
+        {/* <Bar 
                     data={{
                         labels: xLabels,
                         datasets: dataset
                     }}
                     options={options}
                     /> */}
-                </Row>
-
-            </div>
-        </>
-    );
+      </div>
+      {/* </div> */}
+    </>
+  );
 }
 
 export default PlasticStackBarChart;
